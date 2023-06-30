33fa4bcdd084590602a50556862454e31ccbcdf4
sebmuel <mueller.sebastian117@protonmail.com>
2023-06-30 11:54:24 +0200

    implemented mobile images

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // grab all elements
 const stoves = document.querySelectorAll(".stove:not(disable)");
+const imagesContainer = document.querySelectorAll(".stove .stove-img")
 const ENTRANCE_ANIMATION = "animate__fadeIn";
 const OUT_ANIMATION = "animate__fadeOut";
+const MOBILE_BREAKPOINT = 991;
 
 // add the eventlistener with callbacks
 stoves.forEach((stove) => {
@@ -9,6 +11,19 @@ stoves.forEach((stove) => {
   stove.addEventListener("pointerout", handleOut);
   stove.addEventListener("click", handleClick);
 });
+
+
+// add the eventlistener with callbacks
+imagesContainer.forEach((image) => {
+  setImageSrc(image);
+
+})
+
+function setImageSrc(img){
+  if(window.innerWidth <= MOBILE_BREAKPOINT){
+    img.style.backgroundImage = `url('${img.dataset.toggleSrc}')`
+  }
+}
 
 // out callback
 function handleOut() {
